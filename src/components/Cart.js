62ab465aa61cb73bff82f9ebff7dbedd7981284f
Ankit Sharma <ankitsharma290101@gmail.com>
2023-09-03 20:52:11 +0530

    store

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -10,6 +10,7 @@ const Cart=()=>{
 
     }
     const cartItems = useSelector((store)=>store.cart.items)
+    console.log(cartItems)
     return (
         <div className="text-center m-4 p-4">
             <h1 className="text-2xl font-bold"> Cart</h1>
